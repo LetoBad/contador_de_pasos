9174de88d3d79af8322aa4353cdc70b62b7c4424
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2025-07-10 00:07:56 -0300

    layout

--- passos/lib/main.dart
+++ passos/lib/main.dart
@@ -13,12 +13,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => StepsViewModel(),
+      create: (_) => StepsViewModel(),
       child: MaterialApp(
         title: 'Contador de Passos',
-        theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-        home: const HomeView(),
         debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          colorScheme: ColorScheme.dark(
+            primary: Colors.tealAccent[400]!,
+            onPrimary: Colors.black,
+            secondary: Colors.tealAccent[200]!,
+            onSecondary: Colors.black,
+            background: Colors.grey[900]!,
+            surface: Colors.grey[850]!,
+            onSurface: Colors.white,
+          ),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.grey[900],
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.black,
+            elevation: 0,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              color: Colors.tealAccent[400],
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            iconTheme: IconThemeData(color: Colors.tealAccent[400]),
+          ),
+          cardTheme: CardThemeData(
+            color: Colors.grey[850],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            elevation: 4,
+            margin: const EdgeInsets.all(0),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.tealAccent[400],
+              foregroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              elevation: 0,
+            ),
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.tealAccent[400],
+              side: BorderSide(color: Colors.tealAccent[400]!),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 14),
+            ),
+          ),
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(color: Colors.white),
+            displayMedium: TextStyle(color: Colors.white),
+            bodyLarge: TextStyle(color: Colors.white),
+            bodyMedium: TextStyle(color: Colors.white70),
+            labelLarge: TextStyle(color: Colors.black),
+          ),
+          dialogTheme: DialogThemeData(
+            backgroundColor: Colors.grey[850],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16)),
+          ),
+          dividerTheme: DividerThemeData(
+            color: Colors.grey[700],
+            thickness: 1,
+            space: 1,
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.tealAccent[400],
+            foregroundColor: Colors.black,
+          ),
+          progressIndicatorTheme: ProgressIndicatorThemeData(
+            color: Colors.tealAccent[400],
+          ),
+        ),
+        home: const HomeView(),
       ),
     );
   }
--- passos/test/widget_test.dart
+++ passos/test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:passos/main.dart';
+
 import '../lib/main.dart';
 
 void main() {
